65be6d2d45cc855092d432e39dca7ecb92456421
alba-molina-nyc <avmanyc@gmail.com>
2021-12-19 15:51:28 -0500

    drop down for services works

--- script.js
+++ script.js
@@ -22,4 +22,13 @@ document.addEventListener('DOMContentLoaded', function() {
     $('.slider').slider();
   });
 
+  // doprdown
  
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $('.dropdown-trigger').dropdown();
