40c19dc059a7e4d95c1cbd3fef873bd8629181aa
alba-molina-nyc <avmanyc@gmail.com>
2021-12-19 12:12:38 -0500

    headers workseverywhere

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ document.addEventListener('DOMContentLoaded', function() {
   $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+
+  // home aligned captions
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.slider').slider();
+  });
